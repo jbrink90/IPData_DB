be375dd919ac8dc826ca4bd13664f67924757726
jbrink90 <JordanABrinkman@gmail.com>
2025-04-15 14:59:55 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ setuptools.setup(
     name="IPData_DB",
     version="0.0.1",
     author="Jordan Brinkman",
-    author_email="TheConsciousness@github.com",
+    author_email="jbrink90@github.com",
     description="A utility to lookup and store IPData.co IP address results.",
     long_description="ipdata_db allows a user to retrieve information about an IP address without the need to waste API credits.",
     long_description_content_type="text/markdown",
-    url="https://github.com/TheConsciousness/IPData_DB",
+    url="https://github.com/jbrink90/IPData_DB",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
